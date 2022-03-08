ea14eee50b6001964758edb29e6a251a9b9cc86e
Oh seung ik <5seungik@traum.biz>
2022-03-08 19:02:15 +0900

    Practice konva library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Draw from './components/draw/Draw';
 import Main from './components/main/Main';
 import Paint from './components/paint/Paint';
 
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/paint" component={Paint} />
+        <Route path="/draw" component={Draw} />
       </Switch>
     </div>
   );
